c7fe936c4b21bf5ee741e4bfc1490d3abb4a4ed4
Augusto Menabó Izepon <123861778+augustoizepon@users.noreply.github.com>
2023-06-17 17:36:45 -0300

    Adicionando botão para limpar o cache

--- script.js
+++ script.js
@@ -1,75 +1,140 @@
+
+    
 const btnAdd = document.querySelector("#btn-add");
+
 const container = document.querySelector(".container");
 
-const sound = new Audio("IARRU.mp3")
+const sound = new Audio("IARRU.mp3");
 
 const addNewNote = () => {
+
   let title = document.querySelector("#title").value;
+
   let textContentElement = document.querySelector("#text-area").value;
 
   const currentDate = new Date();
+
   const formattedDate = `${currentDate.getDate()}/${
+
     currentDate.getMonth() + 1
+
   }/${currentDate.getFullYear()} ${currentDate.getHours()}:${currentDate.getMinutes()}`;
 
   const note = {
+
     title: title,
+
     textContent: textContentElement,
-    dateTime: formattedDate
+
+    dateTime: formattedDate,
+
   };
 
   let notes = JSON.parse(localStorage.getItem("notes")) || [];
+
   notes.push(note);
+
   localStorage.setItem("notes", JSON.stringify(notes));
 
   const notesGenerated = document.createElement("div");
+
   notesGenerated.className = "notesGenerated";
+
   const titleAndAddGenerated = document.createElement("div");
+
   titleAndAddGenerated.className = "titleAndAddGenerated";
+
   const textArea = document.createElement("div");
+
   textArea.className = "textArea";
+
   notesGenerated.appendChild(titleAndAddGenerated);
+
   notesGenerated.appendChild(textArea);
+
   container.appendChild(notesGenerated);
 
   const dateTimeElement = document.createElement("div");
+
   dateTimeElement.className = "dateTime";
+
   dateTimeElement.textContent = note.dateTime;
+
   notesGenerated.appendChild(dateTimeElement);
 
   titleAndAddGenerated.textContent = note.title;
+
   textArea.textContent = note.textContent;
-  
-  sound.play()
-  
+
+  sound.play();
+
   document.querySelector("#title").value = "";
+
   document.querySelector("#text-area").value = "";
+
 };
 
 const loadNotes = () => {
+
   let notes = JSON.parse(localStorage.getItem("notes")) || [];
 
   notes.forEach((note) => {
+
     const notesGenerated = document.createElement("div");
+
     notesGenerated.className = "notesGenerated";
+
     const titleAndAddGenerated = document.createElement("div");
+
     titleAndAddGenerated.className = "titleAndAddGenerated";
+
     const textArea = document.createElement("div");
+
     textArea.className = "textArea";
+
     notesGenerated.appendChild(titleAndAddGenerated);
+
     notesGenerated.appendChild(textArea);
+
     container.appendChild(notesGenerated);
 
     const dateTimeElement = document.createElement("div");
+
     dateTimeElement.className = "dateTime";
+
     dateTimeElement.textContent = note.dateTime;
+
     notesGenerated.appendChild(dateTimeElement);
 
     titleAndAddGenerated.textContent = note.title;
+
     textArea.textContent = note.textContent;
+
   });
+
 };
 
-
 btnAdd.addEventListener("click", addNewNote);
-loadNotes(); // Carregar as notas ao carregar a página
+
+loadNotes();
+
+const clearCacheButton = document.createElement("button");
+
+clearCacheButton.textContent = "Limpar Cache";
+
+clearCacheButton.style.backgroundColor = "yellow";
+
+clearCacheButton.style.fontFamily = "Arial";
+
+document.body.appendChild(clearCacheButton);
+
+const clearCache = () => {
+
+  localStorage.removeItem("notes");
+
+  location.reload();
+
+};
+
+clearCacheButton.addEventListener("click", clearCache);
+
